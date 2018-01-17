59393d026c8dcfbc22328299bfb77d6f75b4b2b3
QQ <me@quang.be>
2018-01-17 11:53:32 +1100

    Add more transition tests

--- test/rst/transitions_test.clj
+++ test/rst/transitions_test.clj
@@ -2,23 +2,52 @@
   (:require  [midje.sweet :refer :all]
              [rst.core :refer :all]))
 
-(fact "Transitions"
+(fact "Transition between paragraphs"
       (let [lines ["Lorem Ipsum is simply dummy text of"
                    "the printing and typesetting industry."
-                   "==="
-                   "Lorem Ipsum has been the industry's standard dummy text ever since..."]
+                   ""
+                   "===="
+                   ""
+                   "Lorem Ipsum has been the industry's standard..."]
             root (process-document lines)]
-        root => (contains {:type :root :children #(-> % count (= 2))})
-        (let [[first-paragraph second-paragraph] (:children root)]
+        root => (contains {:type :root :children #(-> % count (= 3))})
+        (let [[first-paragraph transition second-paragraph] (:children root)]
           first-paragraph => (contains
                               {:type :paragraph
                                :children (just
                                           [(contains
                                             {:type :text
                                              :value "Lorem Ipsum is simply dummy text of the printing and typesetting industry."})])})
+          transition => (contains {:type :transition})
           second-paragraph => (contains
                                {:type :paragraph
                                 :children (just
                                            [(contains
                                              {:type :text
-                                              :value "Lorem Ipsum has been the industry's standard dummy text ever since..."})])}))))
+                                              :value "Lorem Ipsum has been the industry's standard..."})])}))))
+
+(fact "Error when document ends with a transition"
+      (let [lines ["Lorem Ipsum is simply dummy text of"
+                   "the printing and typesetting industry."
+                   ""
+                   "===="
+                   ""]
+            root (process-document lines)]
+        root => (contains {:type :root :children #(-> % count (= 3))})
+        (let [[first-paragraph transition error] (:children root)]
+          first-paragraph => (contains
+                              {:type :paragraph
+                               :children (just
+                                          [(contains
+                                            {:type :text
+                                             :value "Lorem Ipsum is simply dummy text of the printing and typesetting industry."})])})
+          transition => (contains {:type :transition})
+          error => (contains
+                    {:type :error
+                     :children (just
+                                [(contains
+                                  {:type :paragraph
+                                   :children (just
+                                              [(contains
+                                                {:type :text
+                                                 :value "Document may not end with a transition."})])})])}))))

998888cc7688cdef569eb38bb047b2ad0ff7a55e
QQ <me@quang.be>
2018-01-22 19:05:55 +1100

    Multi-line bullet item test

--- test/rst/bulletlists_tests.clj
+++ test/rst/bulletlists_tests.clj
@@ -2,7 +2,7 @@
   (:require  [midje.sweet :refer :all]
              [rst.core :refer :all]))
 
-(fact "A single line bullet"
+(fact "A single line bullet item"
       (let [lines ["- Lorem Ipsum is simply dummy text"]
             root (process-document lines)]
         root => (contains {:type :root :children #(-> % count (= 1))})
@@ -16,3 +16,28 @@
                                         [(contains
                                           {:type :text
                                            :value "Lorem Ipsum is simply dummy text"})])}))))
+(fact "A multi-line line bullet"
+      (let [lines ["- Lorem Ipsum is simply dummy text"
+                   ""
+                   "  Lorem Ipsum is simply dummy's standard"]
+            root (process-document lines)]
+        root => (contains {:type :root :children #(-> % count (= 1))})
+        (let [list (-> root :children peek)
+              item (-> list :children peek)]
+          list => (contains {:type :bullet-list
+                             :style "-"
+                             :children #(-> % count (= 1))})
+          item => (contains {:type :bullet-item
+                             :children (just
+                                        [(contains
+                                          {:type :paragraph
+                                           :children (just
+                                                      [(contains
+                                                        {:type :text
+                                                         :value "Lorem Ipsum is simply dummy text"})])})
+                                         (contains
+                                          {:type :paragraph
+                                           :children (just
+                                                      [(contains
+                                                        {:type :text
+                                                         :value "Lorem Ipsum is simply dummy's standard"})])})])}))))

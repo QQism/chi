de30ef3ab9e67e2d4494df662bd792e6346ce1e6
QQ <me@quang.be>
2018-01-22 15:01:33 +1100

    Update deps

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "https://quang.be/rst"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.match "0.3.0-alpha5"]
-                 [hiccup "1.0.5"]]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]]
   :profiles {:dev {:dependencies [[midje "1.9.0" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "3.2.1"]
                              [lein-cloverage "1.0.10"]]}})

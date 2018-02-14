85304bd82fc823180db6891fdbe623ac44ba7150
QQ <me@quang.be>
2018-02-14 15:38:49 +1100

    Update paragraph new tests

--- test/rst/paragraph_new_test.clj
+++ test/rst/paragraph_new_test.clj
@@ -1,14 +1,47 @@
 (ns rst.paragraph-new-test
   (:require [clojure.test :refer :all]
-            [rst.core :refer :all]))
+            [rst.test-support :refer :all]
+            [rst.core :refer [process-document]]))
 
 (deftest single-line-paragraph
   (let [lines ["Lorem Ipsum is simply dummy text"]
         root (process-document lines)]
-    (are [fs r] (= ((apply comp fs) root) r)
-      [count :children] 1
-      [#(select-keys % [:type]) first :children] {:type :paragraph}
-      [count :children first :children] 1
-      [#(select-keys % [:type :value]) first :children first :children] {:type :text
-                                                                         :value "Lorem Ipsum is simply dummy text"}
-      )))
+    (assert-node {:type :root :children [:children-count 1 count]} root)
+
+    (let [[paragraph] (:children root)]
+      (assert-node {:type :paragraph
+                      :children [{:type :text
+                                  :value "Lorem Ipsum is simply dummy text"}]}
+                     paragraph))))
+
+(deftest multilines-paragraph
+  (let [lines ["Lorem Ipsum is simply dummy text of"
+               "the printing and typesetting industry."]
+        root (process-document lines)]
+    (assert-node {:type :root :children [:children-count 1 count]} root)
+
+
+    (let [[paragraph] (:children root)]
+      (assert-node {:type :paragraph
+                    :children [{:type :text
+                                :value (str "Lorem Ipsum is simply dummy text of "
+                                            "the printing and typesetting industry.")}]}
+                   paragraph))))
+
+(deftest multi-paragraphs
+  (let [lines ["Lorem Ipsum is simply dummy text of"
+               "the printing and typesetting industry."
+               ""
+               "Lorem Ipsum has been the industry's standard dummy text ever since..."]
+        root (process-document lines)]
+    (assert-node {:type :root :children [:children-count 2 count]} root)
+
+    (let [[paragraph-1 paragraph-2] (:children root)]
+      (assert-node {:type :paragraph :children [{:type :text
+                                                 :value (str "Lorem Ipsum is simply dummy text of "
+                                                             "the printing and typesetting industry.")}]}
+                   paragraph-1)
+      (assert-node {:type :paragraph :children [{:type :text
+                                                 :value (str "Lorem Ipsum has been the industry's "
+                                                             "standard dummy text ever since...")}]}
+                   paragraph-2))))

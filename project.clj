fb7002ec41f96075099a4552b3b7e74fc4d67ea7
QQ <me@quang.be>
2018-02-15 14:13:03 +1100

    Use eftest

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]]
-  :profiles {:dev {:dependencies []
-                   :plugins [[lein-cloverage "1.0.10"]]}})
+  :profiles {:dev {:dependencies [[lein-cljsbuild "1.1.7"]]
+                   :plugins [[lein-cloverage "1.0.10"]
+                             [lein-eftest "0.4.3"]]}})

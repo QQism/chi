80f710d1fda7251614abb9f2c9f6fb0552d37729
QQ <me@quang.be>
2018-02-27 00:08:21 +1100

    Add release build

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]]
+  :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.8"]]
   :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.2"]]
@@ -17,6 +18,16 @@
                                    :source-map "resources/public/js/main.js.map"
                                    :optimizations :advanced
                                    :pretty-print false}}
+                       {:id "release"
+                        :source-paths ["src"]
+                        :compiler {:output-to "release-js/chi.bare.js"
+                                   :optimizations :advanced
+                                   :pretty-print  false
+                                   :elide-asserts true
+                                   :optimize-constants true
+                                   :output-wrapper false
+                                   :parallel-build true}
+                        :notify-command ["release-js/wrap_bare.sh"]}
                        {:id "node-test"
                         :source-paths ["src" "test"]
                         :compiler {:output-to "target/cljsbuild/test.js"
@@ -26,4 +37,7 @@
                                    :main chi.runner
                                    :optimizations :none
                                    :target :nodejs
-                                   :pretty-print true}}]})
+                                   :pretty-print true}}]}
+  :clean-targets ^{:protect false} ["target"
+                                    "release-js/chi.bare.js"
+                                    "release-js/chi.js"])
